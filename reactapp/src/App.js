4b728dba3da36938873bf3562bb873175e347fb3
Jamal Sebbari <sebbarija@gmail.com>
2022-02-28 17:58:04 +0100

    section eleve

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './screens/Home';
-import Student from './screens/Student';
+import Student from './screens/student/Student';
 import Insertion from './screens/Insertion';
 import Volunteer from './screens/Volunteer';
 
@@ -10,7 +10,6 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/decouvrir-l-association" component={Discover} />
         <Route path="/espace-eleves" component={Student} />
         <Route path="/espace-insertion" component={Insertion} />
         <Route path="/espace-benevole" component={Volunteer} />
---
+++ reactapp/src/screens/student/assets/Calendar.js
@@ -0,0 +1,25 @@
+import React, {useState, useEffect} from 'react';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import CalendarPicker from '@mui/lab/CalendarPicker';
+import MonthPicker from '@mui/lab/MonthPicker';
+import YearPicker from '@mui/lab/YearPicker';
+import Grid from '@mui/material/Grid';
+import { fr } from "date-fns/locale";
+
+const minDate = new Date('2020-01-01T00:00:00.000');
+const maxDate = new Date('2034-01-01T00:00:00.000');
+
+export default function Calendar() {
+  const [date, setDate] =useState(new Date());
+
+  const dateFr= ('0'+date.getDate()).slice(-2)+"/"+('0'+(date.getMonth()+1)).slice(-2)+"/"+date.getFullYear()
+  
+  console.log(dateFr);
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns} locale={fr}>
+          <CalendarPicker date={date} onChange={(newDate) => setDate(newDate)} />
+    </LocalizationProvider>
+  );
+}
