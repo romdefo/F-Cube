27abb210e1b5d1bedd2b1df6a415957630ffbf9a
celia <celia.bayet@gmail.com>
2022-03-09 18:04:17 +0100

    blabalal

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./screens/Home";
@@ -20,25 +19,8 @@ const store = createStore(combineReducers({ admin }));
 
 function App() {
   return (
-<<<<<<< HEAD
-    
+    <ThemeProvider theme='theme'>
     <Provider store={store}>
-=======
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Student from './screens/student/Student';
-import Insertion from './screens/Insertion';
-import Volunteer from './screens/benevole/Volunteer';
-import Act from './screens/discover/Act';
-import Admin from './screens/Admin'
-import Discover from './screens/discover/Discover';
-import {ThemeProvider} from '@mui/material';
-import theme from './components/Theme';
-
-function App() {
-  return (
-    <ThemeProvider theme={theme}>   
->>>>>>> finalDiscover
       <Router>
         <Switch>
           <Route exact path="/" component={Discover} />
@@ -50,27 +32,9 @@ function App() {
           <Route path="/admin" component={Admin} />
         </Switch>
       </Router>
-<<<<<<< HEAD
     </Provider>
-=======
-    <ThemeProvider theme={theme}>
-      <Provider store={store}>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Discover} />
-            <Route path="/espace-eleve" component={Student} />
-            <Route path="/espace-insertion" component={Insertion} />
-            <Route path="/espace-benevole" component={Volunteer} />
-            <Route path="/j-agis" component={Act} />
-            <Route path="/admin" component={Admin} />
-          </Switch>
-        </Router>
-      </Provider>
     </ThemeProvider>
->>>>>>> db089bf222f2888a52582f4f684595ecf9745dce
-=======
-    </ThemeProvider>
->>>>>>> finalDiscover
+
   );
 }
 
